8a7e35c4ee4039cf93e33f3668f597304f36b97b
Jin <gbudelmann@gmail.com>
2018-09-04 13:43:30 +1200

    Add basic project and shared scheme so Carthage can import

---
+++ XMLParsing.h
@@ -0,0 +1,19 @@
+//
+//  XMLParsing.h
+//  XMLParsing
+//
+//  Created by Jin Budelmann on 4/09/18.
+//  Copyright © 2018 ShawnMoore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XMLParsing.
+FOUNDATION_EXPORT double XMLParsingVersionNumber;
+
+//! Project version string for XMLParsing.
+FOUNDATION_EXPORT const unsigned char XMLParsingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XMLParsing/PublicHeader.h>
+
+
